df21b0b334a43da5023b9c1afa4b9b1a648b5794
Trend20 <enockomondi305@gmail.com>
2020-06-15 14:29:47 -0400

    input values

--- form.js
+++ form.js
@@ -1,17 +1,3 @@
-const container = document.querySelector('.container');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 
-const loginForm = document.getElementById('login');
-const submitBtn = document.getElementById('button');
-
-submitBtn.addEventListener('click', displayHomePage);
-
-function displayHomePage(){
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML = `
-      <h1>You have logged  in successfully</h1>
-      <button id="button">Proceed</button>
-    `;
-}
