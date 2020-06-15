0dce65af9b9b394a51e8e7dc4c0fc6e8fe6ff17c
Trend20 <enockomondi305@gmail.com>
2020-06-15 14:32:51 -0400

    submit

--- form.js
+++ form.js
@@ -1,3 +1,7 @@
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
+const mainForm = document.getElementById('form');
+const submitInput = document.getElementById('button');
+
+submitInput.addEventListener('click', handleSubmit(e));
