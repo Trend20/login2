5dcd382c3b5f1abdd2a8926d8a9343c129ba8655
Trend20 <enockomondi305@gmail.com>
2020-06-15 15:15:11 -0400

    form events

--- form.js
+++ form.js
@@ -4,4 +4,11 @@ const password = document.getElementById('password');
 const mainForm = document.getElementById('form');
 const submitInput = document.getElementById('button');
 
-submitInput.addEventListener('click', handleSubmit(e));
+submitInput.addEventListener('click', handleSubmit);
+
+ function handleSubmit(e){
+  //  preventDefault form behaviour
+  e.preventDefault();
+  // redirect the user to the welcome page
+  window.location.href = "welcome.html";
+ }
